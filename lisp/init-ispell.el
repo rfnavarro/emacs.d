be47932b72778526bbc59f2bb1687f38c26bfe96
Rafael F. Navarro M. <54745811+rfnavarro@users.noreply.github.com>
2026-01-06 08:50:08 -0500

    Deshabilito cape para corfu por erorres de void-variable

--- lisp/init-ispell.el
+++ lisp/init-ispell.el
@@ -2,10 +2,12 @@
 ;; Debe coincidir con el nombre del archivo descargado (sin extensión)
 (setq ispell-dictionary "es_ES")
 
-(use-package cape
-  :ensure t
-  :init
-  ;; Añade las palabras de ispell/hunspell a las sugerencias de Corfu
-  (add-to-list 'completion-at-point-functions #'cape-ispell))
+;; (use-package cape
+;;   :ensure t
+;;   :demand t
+;;   :init
+;;   ;; Añade las palabras de ispell/hunspell a las sugerencias de Corfu
+;;   (add-to-list 'completion-at-point-functions #'cape-ispell))
+
 
 (provide 'init-ispell)
